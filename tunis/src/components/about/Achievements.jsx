563fa5b8e1d4ff42a2e9ccc77b19da0de8cd4d9e
Suryateja-byte <85501165+Suryateja-byte@users.noreply.github.com>
2025-03-19 22:48:01 -0500

    Update Achievements.jsx

--- tunis/src/components/about/Achievements.jsx
+++ tunis/src/components/about/Achievements.jsx
@@ -4,11 +4,14 @@ const achievementsContent = [
   {
     title: "5+",
     subTitle1: "years of",
-    subTitle2: "experience in Android Development",
+    subTitle2: "experience in UI/UX Design", // Changed from Android
   },
   { title: "10+", subTitle1: "successfully delivered", subTitle2: "projects" },
-  { title: "3+", subTitle1: "diverse", subTitle2: "industries served" },
+  { title: "2+", // Changed from 3 to 2
+   subTitle1: "industries",
+   subTitle2: "served" }, //made subtitle singular
   { title: "2", subTitle1: "degrees in", subTitle2: "Computer Science" },
+  { title: "45%", subTitle1: "increase in", subTitle2: "user satisfaction" }, //added new achievement
 ];
 
 const Achievements = () => {
